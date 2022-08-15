a6d49926ad1ea185e92754957d18eb37a932ee5c
olexanderf <110126454+olexanderf@users.noreply.github.com>
2022-08-15 21:08:11 +0300

    add fav icon, open graph, js carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        navigation : true,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:3000,
+        loop: true,
+    });
 });
